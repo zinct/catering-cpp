5aea2b1d4fbb561fde85e43be42a123ff0d84d9d
Indra Mahesa <indramahesa128@gmail.com>
2023-12-16 10:59:57 +0700

    Initial Commit

---
+++ catering/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  catering
+//
+//  Created by Indra Mahesa on 16/12/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
